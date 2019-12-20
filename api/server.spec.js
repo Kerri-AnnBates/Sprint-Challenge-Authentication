68f7507ca8beadb1fc7c609afba77e7e4dd4937c
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-20 14:39:29 -0500

    completed tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -50,28 +50,30 @@ describe('server.js', () => {
                done();
             });
       });
-      // it('respond 200 OK status', async function () {
-         it('succeeds with correct credentials', async () => {
-            const demoUser = {username: 'john', password: 'test'}
-            const response = await request(server).post(`/api/auth/login`, demoUser)
-               .expect(200);
-            expect(res.body.username).toBe(demoUser.username);
-         });
+
+      it('succeeds with correct credentials and 200 OK', () => {
+         // const demoUser = { username: 'john', password: 'test' }
+         request(server)
+            .post(`/api/auth/login`)
+            .send({ username: 'john', password: 'test' })
+            .then(res => {
+               expect(res.status).toBe(200);
+               expect(res.body.message).toBe("Welcome john");
+            });
       });
-   // });
+
+   });
 
    describe('POST /api/auth/register', function () {
-      it('responds with json and 201 CREATED status', function (done) {
+      it('responds with json and 201 CREATED status', function () {
          request(server)
             .post('/api/auth/register')
-            .send({ username: 'john', password: 'test' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/gi)
-            .expect(201)
-            .end(function (err, res) {
-               if (err) return done(err);
-               done();
-            });
+            .send({ username: 'ben', password: 'dummy' })
+            // .set('Accept', 'application/json')
+            .then(res => {
+               expect(res.status).toBe(201);
+               expect(res.type, /json/gi)
+            })
       });
    });
 
